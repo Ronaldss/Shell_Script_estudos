8a30db2e92866d2fde0b43c730d6add4fc36b48f
Ronald <ronaldcontact2019@gmail.com>
2023-09-14 20:52:26 -0300

    Iniciando com Shell Script - parte 1

---
+++ hora.sh
@@ -0,0 +1,14 @@
+#!@bin/bash
+echo "----------------------------------"
+echo
+NOME="Ronald"
+HORA=`date +'%H'`
+MIN=`date +'%M'`
+
+echo "NOME: $NOME"
+echo "HORA: $HORA"
+echo "MINUTOS: $MIN"
+echo
+echo "Agora sao $HORA hora(s) e $MIN minuto(s)." 
+echo
+echo "----------------------------------"
